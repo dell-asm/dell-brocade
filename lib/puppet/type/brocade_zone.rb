10ea0f050733e4d978a5c7c72c4d2b26dec5758f
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2013-12-18 13:10:26 +0000

    Added a new property zoneconfig

--- lib/puppet/type/brocade_zone.rb
+++ lib/puppet/type/brocade_zone.rb
@@ -9,6 +9,7 @@ Puppet::Type.newtype(:brocade_zone) do
     desc "Zone name"
     isnamevar
     newvalues(/^\S+$/)
+
     validate do |value|
       if value.strip.length == 0
         raise ArgumentError, "Invalid zone name."
@@ -19,6 +20,17 @@ Puppet::Type.newtype(:brocade_zone) do
   newparam(:member) do
     desc "memeber added in the zone"
     newvalues(/^\S+$/)
-  end
-end
 
+    validate do |value|
+      if value.strip.length == 0
+        raise ArgumentError, "Invalid zone name."
+      end
+    end
+  end
+
+  newparam(:zoneconfig) do
+    desc "zonecofig in which zone has to be added"
+    newvalues(/^\S+$/)
+  end
+
+end
