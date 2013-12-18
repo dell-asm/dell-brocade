d688ffa303875923401de899f76b3eccb1d3965c
sanjeev rohila <sanjeev_rohila@dell.com>
2013-12-18 18:33:35 +0000

    Changes after the code analysis through tools.

--- lib/puppet/provider/brocade_config/brocade_config.rb
+++ lib/puppet/provider/brocade_config/brocade_config.rb
@@ -6,14 +6,15 @@ Puppet::Type.type(:brocade_config).provide(:brocade_config, :parent => Puppet::P
 
   def create
     response = String.new("")
-    response = @device.transport.command("cfgshow #{@resource[:configname]}", :noop => false)
+    self.transport
+    response = @transport.command("cfgshow #{@resource[:configname]}", :noop => false)
     if ( response.include? "does not exist" ) && ( "#{@resource[:configstate]}" == "enable" )
       @resource.provider.configcreate
       @resource.provider.configenable
     end
 
     if ( response.include? "does not exist" ) && ("#{@resource[:configstate]}" == "disable")
-      @resource.provider.configdisable
+      @resource.provider.configcreate
     end
 
     if (!response.include? "does not exist" ) && ( "#{@resource[:configstate]}" == "enable" )  
@@ -23,18 +24,17 @@ Puppet::Type.type(:brocade_config).provide(:brocade_config, :parent => Puppet::P
     if (!response.include? "does not exist" ) && ( "#{@resource[:configstate]}" == "disable")
       @resource.provider.configdisable
     end
-
   end
 
   def destroy
     response = String.new("")
     Puppet.debug("Deleting Config #{@resource[:configname]}")
-    response = @device.transport.command("cfgdelete  #{@resource[:configname]}", :noop => false)
+    response = @transport.command("cfgdelete  #{@resource[:configname]}", :noop => false)
     if !response.include? "should not be deleted."
       Puppet.debug(response)
       if !response.include? "not found"
-        @device.transport.command("cfgsave", :prompt => /Do/)
-        @device.transport.command("yes", :noop => false)
+        @transport.command("cfgsave", :prompt => /Do/)
+        @transport.command("yes", :noop => false)
       end
     else 
       Puppet.debug(response)
@@ -44,18 +44,18 @@ Puppet::Type.type(:brocade_config).provide(:brocade_config, :parent => Puppet::P
   def configcreate 
     response = String.new("")
     Puppet.debug("Creating Config #{@resource[:configname]} with members #{@resource[:member_zone]}")
-    response =  @device.transport.command("cfgcreate  #{@resource[:configname]},  \"#{@resource[:member_zone]}\"", :noop => false)
+    response =  @transport.command("cfgcreate  #{@resource[:configname]},  \"#{@resource[:member_zone]}\"", :noop => false)
     #Puppet.debug("Puppet::Provider::zone_config: response #{response}. \n")
     if !response.include? "duplicate name" 
-      @device.transport.command("cfgsave", :prompt => /Do/)
-      @device.transport.command("yes", :noop => false)
+      @transport.command("cfgsave", :prompt => /Do/)
+      @transport.command("yes", :noop => false)
     end
   end
 
   def configenable
     response = String.new("")
-    @device.transport.command("cfgenable #{@resource[:configname]}", :prompt => /Do you want to enable/)
-    response = @device.transport.command("yes", :noop => false)
+    @transport.command("cfgenable #{@resource[:configname]}", :prompt => /Do you want to enable/)
+    response = @transport.command("yes", :noop => false)
     if !response.include? "not found"
       puts "Config: #{@resource[:configname]} does not exist."
     end
@@ -63,17 +63,15 @@ Puppet::Type.type(:brocade_config).provide(:brocade_config, :parent => Puppet::P
   
   def configdisable
     response = String.new("")
-    @device.transport.command("cfgDisable", :prompt => /Do you want to disable /)
-    @device.transport.command("yes", :noop => false)
+    @transport.command("cfgDisable", :prompt => /Do you want to disable /)
+    @transport.command("yes", :noop => false)
   end
 
   def exists?
-    self.transport
+    self.device_transport
     response = String.new("")
-    response = @device.transport.command("cfgshow #{@resource[:configname]}", :noop => false)
-    
+    response = @transport.command("cfgshow #{@resource[:configname]}", :noop => false)
     if "#{@resource[:ensure]}" == "present" 
-      puts "here" 
       false
     else
       true
