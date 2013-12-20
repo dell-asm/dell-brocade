56a2cd5026d3e20374b4cea5bfd5d6cb96d71352
sangeeta sharma <sangeeta_sharma@dell.com>
2013-12-20 10:58:18 +0000

    Modified for documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,15 @@
+# Class: brocade
+#
+# This module manages compellent
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
 class brocade {
 }
