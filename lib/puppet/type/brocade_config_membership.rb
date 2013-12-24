94aa85535b550b056e1a96b6835c67b8605e52cc
sanjeev rohila <sanjeev_rohila@dell.com>
2013-12-24 13:26:39 +0000

    Validation added for checking the special characters for the zone name parameter.Will allow only an underscore.

--- lib/puppet/type/brocade_config_membership.rb
+++ lib/puppet/type/brocade_config_membership.rb
@@ -1,4 +1,3 @@
-[root@puppet-centos type]# vi brocade_config_membership.rb
 Puppet::Type.newtype(:brocade_config_membership) do
   @doc = "This represents a zone config membership on a brocade switch."
 
@@ -23,7 +22,7 @@ Puppet::Type.newtype(:brocade_config_membership) do
         raise ArgumentError, "Enter a valid Config name value."
       end
       if ( value =~ /[\W]+/ )
-        raise ArgumentError, "Brocade does not support special characters in Config name."
+        raise ArgumentError, "Brocade does not support speacial characters in Config name."
       end
     end
   end
@@ -31,10 +30,15 @@ Puppet::Type.newtype(:brocade_config_membership) do
   newproperty(:member_zone) do
     desc "zone added in the config"
     newvalues(/^\S+$/)
-    value.split(";").each do |line|
-      item = line.strip
-      if ( item =~ /[\W]+/ )
-        raise ArgumentError, "Brocade does not support special characters in Zone name."
+    validate do |value|
+      if value.strip.length == 0
+        raise ArgumentError, "Zone name value can not be blank."
+      end
+      value.split(";").each do |line|
+        item = line.strip
+        if ( item =~ /[\W]+/ )
+          raise ArgumentError, "Brocade does not support special characters in Zone name."
+        end
       end
     end
   end
