131ae38f27789997bfb55db6f3f4bc6935458303
sangeeta sharma <sangeeta_sharma@dell.com>
2013-12-24 09:23:12 +0000

    Added support for message externalization.

---
+++ lib/puppet/provider/brocade_messages.rb
@@ -0,0 +1,11 @@
+module Puppet::Provider::Brocade_messages
+
+        ZONE_MSG_01="Puppet::Provider::brocade_zone: A Brocade zone with zonename: %s, zonemember:  %s is being added."
+        ZONE_MSG_02="Puppet::Provider::brocade_zone: A Brocade zone with zonename: %s is being deleted."
+		ZONE_MSG_03="Puppet::Provider::brocade_zone: Verifying whether or not a Brocade zone with zonename: %s exists."
+
+        RESPONSE_ZONE_01="not found"
+        RESPONSE_ZONE_02="does not exist."
+		RESPONSE_ZONE_03="duplicate name"
+
+end
--- lib/puppet/provider/brocade_zone/brocade_zone.rb
+++ lib/puppet/provider/brocade_zone/brocade_zone.rb
@@ -1,4 +1,5 @@
 require 'puppet/provider/brocade_fos'
+require 'puppet/provider/brocade_messages'
 
 Puppet::Type.type(:brocade_zone).provide(:brocade_zone, :parent => Puppet::Provider::Brocade_fos) do
   @doc = "Manage brocade zone creation, modification and deletion."
@@ -6,9 +7,9 @@ Puppet::Type.type(:brocade_zone).provide(:brocade_zone, :parent => Puppet::Provi
  mk_resource_methods
 
  def create
-    Puppet.debug("Puppet::Provider::brocade_zone: A Brocade zone with zonename: #{@resource[:zonename]}, zonemember:  #{@resource[:member]} is being added.")
+    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MSG_01%[@resource[:zonename],@resource[:member]])
     response =  @transport.command("zonecreate  #{@resource[:zonename]}, \"#{@resource[:member]}\"", :noop => false)
-    if !response.include? "duplicate name"
+    if !response.include? Puppet::Provider::Brocade_messages::RESPONSE_ZONE_03
       cfg_save
     end
   end
@@ -16,18 +17,18 @@ Puppet::Type.type(:brocade_zone).provide(:brocade_zone, :parent => Puppet::Provi
 
 
   def destroy
-    Puppet.debug("Puppet::Provider::brocade_zone: A Brocade zone with zonename: #{@resource[:zonename]} is being deleted.")
+    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MSG_02%[@resource[:zonename]])
     response = @transport.command("zonedelete  #{@resource[:zonename]}", :noop => false)
-    if !response.include? "not found"
+    if !response.include? Puppet::Provider::Brocade_messages::RESPONSE_ZONE_01
       cfg_save
     end
   end
 
   def exists?
-    Puppet.debug("Puppet::Provider::brocade_zone: Verifying whether or not a Brocade zone with zonename: #{@resource[:zonename]} exists.")
+    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MSG_03%[@resource[:zonename]])
     self.device_transport
     response =  @transport.command("zoneshow #{@resource[:zonename]}", :noop => false)
-    if !response.include? "does not exist."
+    if !response.include? Puppet::Provider::Brocade_messages::RESPONSE_ZONE_02
       true
     else
       false
