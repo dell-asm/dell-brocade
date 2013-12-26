1504c636b1570cddb996ff3bdeb4b68e283a41ba
abhishek grover <abhishek_grover@dell.com>
2013-12-26 13:14:54 +0000

    updated Response file

--- lib/puppet/provider/brocade_responses.rb
+++ lib/puppet/provider/brocade_responses.rb
@@ -3,5 +3,10 @@ module Puppet::Provider::Brocade_responses
         RESPONSE_NOT_FOUND="not found"
         RESPONSE_DOES_NOT_EXIST="does not exist."
 	RESPONSE_DUPLICATE_NAME="duplicate name"
+	RESPONSE_ALREADY_CONTAINS="already contains"
+	RESPONSE_INVALID_PARAMETERS="Invalid Parameters"
+	RESPONSE_IS_NOT_IN="is not in"
+	RESPONSE_SHOULD_NOT_BE_DELETED="should not be deleted"
+	RESPONSE_INVALID="invalid"
 
 end
