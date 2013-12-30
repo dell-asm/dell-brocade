9c15047ceebeb268922f73b64047a905537bb9c7
anuj mittal <anuj_mittal@dell.com>
2013-12-30 10:18:56 +0000

    Changes as per roody

--- lib/puppet/provider/brocade_alias/brocade_alias.rb
+++ lib/puppet/provider/brocade_alias/brocade_alias.rb
@@ -11,7 +11,7 @@ Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Pro
  def create
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_CREATE_DEBUG%[@resource[:alias_name],@resource[:member]])
     response = @transport.command("alicreate #{@resource[:alias_name]}, \"#{@resource[:member]}\"", :noop => false)
-    if !(response.include? Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG || response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID_NAME)
+    if !((response.include? Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG )|| (response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID_NAME))
 	  cfg_save
 	elsif(response.include? Puppet::Provider::Brocade_responses::RESPONSE_DUPLICATE_NAME)
 	  Puppet.info(Puppet::Provider::Brocade_messages::ALIAS_ALREADY_EXIST_INFO%[@resource[:alias_name]])
@@ -23,7 +23,7 @@ Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Pro
   def destroy
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_DESTROY_DEBUG%[@resource[:alias_name]])
     response = @transport.command("alidelete  #{@resource[:alias_name]}", :noop => false)
-    if !response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND
+    if !(response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND)
        cfg_save
 	else
 	   Puppet.info(Puppet::Provider::Brocade_messages::ALIAS_DOES_NOT_EXIST_INFO%[@resource[:alias_name]])
@@ -34,7 +34,7 @@ Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Pro
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_EXIST_DEBUG%[@resource[:alias_name]])
     self.device_transport
     response = @transport.command("alishow #{@resource[:alias_name]}", :noop => false)
-    if !response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST
+    if !(response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
       true
     else
       false
