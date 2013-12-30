425e58b81e6eb0ea350c1054531a423ca04188f8
anuj mittal <anuj_mittal@dell.com>
2013-12-30 09:25:08 +0000

    Fixed error for name too long and invalid name

--- lib/puppet/provider/brocade_alias/brocade_alias.rb
+++ lib/puppet/provider/brocade_alias/brocade_alias.rb
@@ -11,8 +11,12 @@ Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Pro
  def create
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_CREATE_DEBUG%[@resource[:alias_name],@resource[:member]])
     response = @transport.command("alicreate #{@resource[:alias_name]}, \"#{@resource[:member]}\"", :noop => false)
-    if !response.include? Puppet::Provider::Brocade_responses::RESPONSE_DUPLICATE_NAME
-      cfg_save
+    if !(response.include? Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG || response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID_NAME)
+     cfg_save
+	elsif !response.include? Puppet::Provider::Brocade_responses::RESPONSE_DUPLICATE_NAME
+	 Puppet.info(Puppet::Provider::Brocade_messages::ALIAS_ALREADY_EXIST_INFO%[@resource[:alias_name]])
+	else
+	 raise Puppet::Error, Puppet::Provider::Brocade_messages::ALIAS_CREATE_ERROR %[@resource[:alias_name],response]
     end
   end
 
