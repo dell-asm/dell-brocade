bae534a6536b0b1d6344b911af47df6553db571c
abhishek grover <abhishek_grover@dell.com>
2013-12-30 10:10:57 +0000

    Refactored after code analysis using Metric fu

--- lib/puppet/provider/brocade_config_membership/brocade_config_membership.rb
+++ lib/puppet/provider/brocade_config_membership/brocade_config_membership.rb
@@ -2,12 +2,7 @@ require 'puppet/provider/brocade_fos'
 require 'puppet/provider/brocade_responses'
 require 'puppet/provider/brocade_messages'
 
-Puppet::Type.type(:brocade_config_membership).provide(:brocade_config_membership, :parent => Puppet::Provider::Brocade_fos) do
-  @doc = "Manage addition and removal of zones to config."
-  mk_resource_methods
-
-  def create
-    Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_CREATE_DEBUG%[@resource[:member_zone],@resource[:configname]])
+  def create_local
     response = @transport.command("cfgadd #{@resource[:configname]}, \"#{@resource[:member_zone]}\"", :noop => false)
     if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND ) || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID_PARAMETERS)
       raise Puppet::Error, Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_CREATE_ERROR%[@resource[:member_zone],@resource[:configname],response]
@@ -18,9 +13,7 @@ Puppet::Type.type(:brocade_config_membership).provide(:brocade_config_membership
     end
   end
 
-
-  def destroy
-    Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_DESTORY_DEBUG%[@resource[:member_zone],@resource[:configname]])
+  def destroy_local
     response = @transport.command("cfgremove #{@resource[:configname]}, \"#{@resource[:member_zone]}\"", :noop => false)
     if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND )
       raise Puppet::Error, Puppet::Provider::Brocade_responses::CONFIG_MEMBERSHIP_DESTROY_ERROR%[@resource[:member_zone],@resource[:configname],response]
@@ -29,6 +22,21 @@ Puppet::Type.type(:brocade_config_membership).provide(:brocade_config_membership
     else 
       cfg_save
     end
+  end
+
+Puppet::Type.type(:brocade_config_membership).provide(:brocade_config_membership, :parent => Puppet::Provider::Brocade_fos) do
+  @doc = "Manage addition and removal of zones to config."
+  mk_resource_methods
+
+  def create
+    Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_CREATE_DEBUG%[@resource[:member_zone],@resource[:configname]])
+	create_local
+  end
+
+
+  def destroy
+    Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_DESTORY_DEBUG%[@resource[:member_zone],@resource[:configname]])
+	destroy_local
   end
 
 
