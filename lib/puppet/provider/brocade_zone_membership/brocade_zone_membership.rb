835c8afcd09a1f40e8be2eb1b6447fdea6ff9bec
abhishek grover <abhishek_grover@dell.com>
2013-12-30 09:05:12 +0000

    Refactored after code analysis using Metric fu

--- lib/puppet/provider/brocade_zone_membership/brocade_zone_membership.rb
+++ lib/puppet/provider/brocade_zone_membership/brocade_zone_membership.rb
@@ -2,16 +2,14 @@ require 'puppet/provider/brocade_fos'
 require 'puppet/provider/brocade_responses'
 require 'puppet/provider/brocade_messages'
 
+def check_error_cond(response)
+return ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND ) || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID)  || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
+end
 
-Puppet::Type.type(:brocade_zone_membership).provide(:brocade_zone_membership, :parent => Puppet::Provider::Brocade_fos) do
-  @doc = "Manage brocade zone member addition and removal from/to zone."
-
-  mk_resource_methods
-  def create
-    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_CREATE_DEBUG%[@resource[:member],@resource[:zonename]])
+  def create_local
     response = String.new("")
     response = @transport.command("zoneadd #{@resource[:zonename]},\"#{@resource[:member]}\"", :noop => false)
-    if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND ) || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID)  || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
+    if check_error_cond(response)
       raise Puppet::Error, Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_CREATE_ERROR%[@resource[:member],@resource[:zonename],response]
     elsif response.include? Puppet::Provider::Brocade_responses::RESPONSE_ALREADY_CONTAINS
       Puppet.info(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_ALREADY_EXIST_INFO%[@resource[:member],@resource[:zonename]])
@@ -20,19 +18,32 @@ Puppet::Type.type(:brocade_zone_membership).provide(:brocade_zone_membership, :p
     end
   end
 
-  def destroy
-    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_DESTROY_DEBUG%[@resource[:member],@resource[:zonename]])
-
+  def destroy_local
     response = String.new("")
     response =  @transport.command("zoneremove #{@resource[:zonename]},\"#{@resource[:member]}\"", :noop => false)
-    if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
-      raise Puppet::Error, Puppet::Provider::Brocade_responses::ZONE_MEMBERSHIP_DESTROY_ERROR%[@resource[:member],@resource[:zonename],response]
-	elsif (response.include? Puppet::Provider::Brocade_responses::RESPONSE_IS_NOT_IN )
-	  Puppet.info(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_ALREADY_REMOVED_INFO%[@resource[:member],@resource[:zonename]])
-    else 
+    if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST) || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND )
+      raise Puppet::Error, Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_DESTROY_ERROR%[@resource[:member],@resource[:zonename],response]
+        elsif (response.include? Puppet::Provider::Brocade_responses::RESPONSE_IS_NOT_IN )
+          Puppet.info(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_ALREADY_REMOVED_INFO%[@resource[:member],@resource[:zonename]])
+    else
       cfg_save
-end
-end
+        end
+	end
+	
+Puppet::Type.type(:brocade_zone_membership).provide(:brocade_zone_membership, :parent => Puppet::Provider::Brocade_fos) do
+  @doc = "Manage brocade zone member addition and removal from/to zone."
+
+  mk_resource_methods
+
+	def create
+    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_CREATE_DEBUG%[@resource[:member],@resource[:zonename]])
+		create_local
+	end 
+
+    def destroy
+    Puppet.debug(Puppet::Provider::Brocade_messages::ZONE_MEMBERSHIP_DESTROY_DEBUG%[@resource[:member],@resource[:zonename]])
+		destroy_local
+	end
 
   def exists?
     self.device_transport
@@ -43,3 +54,4 @@ end
 end
 end
 end
+
