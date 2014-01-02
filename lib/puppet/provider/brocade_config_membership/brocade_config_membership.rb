b26fac203df97285ecee1ee54380011e32943892
sanjeev rohila <sanjeev_rohila@dell.com>
2014-01-02 05:07:08 +0000

    Re factored code and method renamed as per their functionality and the code indention correction.

--- lib/puppet/provider/brocade_config_membership/brocade_config_membership.rb
+++ lib/puppet/provider/brocade_config_membership/brocade_config_membership.rb
@@ -2,7 +2,7 @@ require 'puppet/provider/brocade_fos'
 require 'puppet/provider/brocade_responses'
 require 'puppet/provider/brocade_messages'
 
-  def create_config_membership
+  def config_add_zone
     response = @transport.command("cfgadd #{@resource[:configname]}, \"#{@resource[:member_zone]}\"", :noop => false)
     if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND ) || ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID_PARAMETERS)
       raise Puppet::Error, Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_CREATE_ERROR%[@resource[:member_zone],@resource[:configname],response]
@@ -13,40 +13,37 @@ require 'puppet/provider/brocade_messages'
     end
   end
 
-  def destroy_config_membership
+  def config_remove_zone
     response = @transport.command("cfgremove #{@resource[:configname]}, \"#{@resource[:member_zone]}\"", :noop => false)
     if ( response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND )
       raise Puppet::Error, Puppet::Provider::Brocade_responses::CONFIG_MEMBERSHIP_DESTROY_ERROR%[@resource[:member_zone],@resource[:configname],response]
-	elsif (response.include? Puppet::Provider::Brocade_responses::RESPONSE_IS_NOT_IN )
-	  Puppet.info(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_ALREADY_REMOVED_INFO%[@resource[:member_zone],@resource[:configname]])
+    elsif (response.include? Puppet::Provider::Brocade_responses::RESPONSE_IS_NOT_IN )
+      Puppet.info(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_ALREADY_REMOVED_INFO%[@resource[:member_zone],@resource[:configname]])
     else 
       cfg_save
     end
   end
 
-Puppet::Type.type(:brocade_config_membership).provide(:brocade_config_membership, :parent => Puppet::Provider::Brocade_fos) do
-  @doc = "Manage addition and removal of zones to config."
-  mk_resource_methods
+  Puppet::Type.type(:brocade_config_membership).provide(:brocade_config_membership, :parent => Puppet::Provider::Brocade_fos) do
+    @doc = "Manage addition and removal of zones to config."
+    mk_resource_methods
 
-  def create
-    Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_CREATE_DEBUG%[@resource[:member_zone],@resource[:configname]])
-	create_config_membership
-  end
+    def create
+      Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_CREATE_DEBUG%[@resource[:member_zone],@resource[:configname]])
+      config_add_zone 
+    end
 
+    def destroy
+      Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_DESTORY_DEBUG%[@resource[:member_zone],@resource[:configname]])
+      config_remove_zone 
+    end
 
-  def destroy
-    Puppet.debug(Puppet::Provider::Brocade_messages::CONFIG_MEMBERSHIP_DESTORY_DEBUG%[@resource[:member_zone],@resource[:configname]])
-	destroy_config_membership
-  end
-
-
-  def exists?
-    self.device_transport
-    if "#{@resource[:ensure]}" == "present" 
-      false
-    else
-      true
+    def exists?
+      self.device_transport
+      if "#{@resource[:ensure]}" == "present" 
+        false
+      else
+        true
+      end
     end
   end
-
-end
