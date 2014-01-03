40c3b44f2a049f450a7f9e0bbc765bfb667e9750
manoj kumar23 <manoj_kumar23@dell.com>
2014-01-03 07:32:20 +0000

    Unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,7 @@ $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..','lib'
 RSpec.configure do |c|
   c.module_path = module_path
   c.manifest_dir = File.join(module_path, 'manifests')
-  puts "module path #{module_path}"
-  
+    
   if Puppet::Util::Platform.windows?
     c.output_stream = $stdout
     c.error_stream = $stderr
--- spec/unit/puppet/provider/brocade_zone/brocade_zone_spec.rb
+++ spec/unit/puppet/provider/brocade_zone/brocade_zone_spec.rb
@@ -6,35 +6,46 @@ require 'puppet/provider/brocade_fos'
 require 'puppet/util/network_device/brocade_fos/device'
 require 'puppet/util/network_device/base_fos'
 require 'rspec/mocks'
+require 'puppet/provider/brocade_fos'
+require 'puppet/provider/brocade_responses'
+require 'puppet/provider/brocade_messages'
 
 describe Puppet::Type.type(:brocade_zone).provider(:brocade_zone) do
-  
 
-context "when brocade_zone provider is created " do
-  it "should have create method defined for brocade_zone" do
-  described_class.instance_method(:create).should_not == nil
+  context "when brocade_zone provider is created " do
+    it "should have create method defined for brocade_zone" do
+      described_class.instance_method(:create).should_not == nil
+
+    end
+
+    it "should have destroy method defined for brocade_zone" do
+      described_class.instance_method(:destroy).should_not == nil
+    end
+
+    it "should have exists? method defined for brocade_zone" do
+      described_class.instance_method(:exists?).should_not == nil
+
+    end
   end
 
-  it "should have destroy method defined for brocade_zone" do
-    described_class.instance_method(:destroy).should_not == nil
-  end
-
-  it "should have exists? method defined for brocade_zone" do
-     described_class.instance_method(:exists?).should_not == nil
-
-  end
-end
-
-context "#create"
+  context "#create"
   it "should throw error if response is RESPONSE_INVALID or RESPONSE_NAME_TOO_LONG" do
-    dummy_transport=double("transport")
-  described_class.transport=(dummy_transport)
-  end    
-  
+    dummy_transport=double('transport')
+    dummy_transport.stub(:command).and_return (Puppet::Provider::Brocade_responses::RESPONSE_INVALID)
+    dummy_resource = double ("resource")
+    dummy_resource.stub(:[]).and_return ("")
 
-it "should warn if brocade zone name is already exist"
+   
+    provider_object = described_class.new
+    provider_object.instance_variable_set(:@resource, dummy_resource)
+    provider_object.instance_variable_set(:@transport, dummy_transport)
 
-it "should save configuration  if brocade zone is created successfully"
+    expect {provider_object.create}.to raise_error(Puppet::Error)
+  end
+
+  it "should warn if brocade zone name is already exist"
+
+  it "should save configuration  if brocade zone is created successfully"
 
 end
 
