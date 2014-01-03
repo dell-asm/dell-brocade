28adb7f15c62bc7054939ec73b1891648d8392bd
manoj kumar23 <manoj_kumar23@dell.com>
2014-01-03 05:29:08 +0000

    Include path updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,21 @@
 require 'rspec-puppet'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+module_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..','lib'))
 
 RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.module_path = module_path
+  c.manifest_dir = File.join(module_path, 'manifests')
+  puts "module path #{module_path}"
+  
+  if Puppet::Util::Platform.windows?
+    c.output_stream = $stdout
+    c.error_stream = $stderr
+
+    c.formatters.each do |f|
+      if not f.instance_variable_get(:@output).kind_of?(::File)
+        f.instance_variable_set(:@output, $stdout)
+      end
+    end
+  end
 end
