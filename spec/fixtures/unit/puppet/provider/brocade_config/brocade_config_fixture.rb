e4899ffd221ed724471b433de785c8989f72a811
anuj mittal <anuj_mittal@dell.com>
2014-01-06 12:15:31 +0000

    Fixture for brocade_config provider.

---
+++ spec/fixtures/unit/puppet/provider/brocade_config/brocade_config_fixture.rb
@@ -0,0 +1,32 @@
+class Brocade_config_fixture
+
+  attr_accessor :brocade_config, :provider
+
+  def initialize
+    @brocade_config = get_brocade_config	
+	@provider = brocade_config.provider	
+  end
+  
+  private 
+  def  get_brocade_config
+    Puppet::Type.type(:brocade_config).new(
+    :configname => 'DemoConfig',
+    :ensure => 'present',
+    :member_zone => 'DemoZone',
+	:configstate => 'disable',
+    )
+  end
+  
+  public
+  def get_config_name
+    brocade_config[:configname]
+  end
+ 
+   def enable_config
+     brocade_config[:configstate] =>'enable'
+   end
+   
+   def disable_config
+     brocade_config[:configstate] =>'disable'
+   end
+end
