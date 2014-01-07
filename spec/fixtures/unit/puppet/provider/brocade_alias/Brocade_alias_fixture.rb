66e44cc8233ea718c4cbef3cb2c90d97d1e44c8d
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2014-01-07 06:58:13 +0000

    Fixture class for brocade alias provider type

---
+++ spec/fixtures/unit/puppet/provider/brocade_alias/Brocade_alias_fixture.rb
@@ -0,0 +1,24 @@
+class Brocade_alias_fixture
+
+  attr_accessor :brocade_alias, :provider
+
+  def initialize
+    @brocade_alias = get_brocade_alias
+	@provider = brocade_alias.provider	
+  end
+  
+  private 
+  def  get_brocade_alias
+    Puppet::Type.type(:brocade_alias).new(
+    :alias_name => 'DemoAlias',
+    :ensure => 'present',
+    :member => '0f:0a:0a:0a:0a:0a:0a:0a'
+	)
+  end
+  
+  public
+  def get_alias_name
+    brocade_alias[:alias_name]
+  end
+end
+  
