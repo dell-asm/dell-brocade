afbee49d139d793a93848aa01289cfeaf9d6d58b
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2014-01-07 07:01:11 +0000

    Brocade alias create rspec unit test

--- spec/unit/puppet/provider/brocade_alias/brocade_alias_spec.rb
+++ spec/unit/puppet/provider/brocade_alias/brocade_alias_spec.rb
@@ -1,15 +1,19 @@
 #! /usr/bin/env ruby
-
 require 'spec_helper'
-require 'yaml'
-require 'puppet/provider/brocade_fos'
-require 'puppet/util/network_device/brocade_fos/device'
-require 'puppet/util/network_device/base_fos'
-require 'rspec/mocks'
-require 'puppet/provider/brocade_responses'
-require 'puppet/provider/brocade_messages'
+require 'fixtures/unit/puppet/provider/brocade_alias/Brocade_alias_fixture'
+
+NOOPS_HASH = {:noop => false}
 
 describe Puppet::Type.type(:brocade_alias).provider(:brocade_alias) do
+
+before(:each) do
+    @fixture = Brocade_alias_fixture.new
+    dummy_transport=double('transport')
+    dummy_transport.stub(:command)
+    @fixture.provider.transport = dummy_transport
+    @fixture.provider.stub(:cfg_save)
+
+  end
 
   context "when brocade_zone provider is created " do
     it "should have create method defined for brocade_zone" do
@@ -29,45 +33,34 @@ describe Puppet::Type.type(:brocade_alias).provider(:brocade_alias) do
 
   context "when brocade alias is created" do
     it "should raise error if response contains 'invalid' while creating brocade alias" do
-	 #Given
-	  fixture = Brocade_alias_fixture.new
-	  ops_hash = {:noop => false}
-	  dummy_transport=double('transport')
-	  fixture.provider.transport = dummy_transport 
-	 
-	 #When-Then
-      dummy_transport.should_receive(:command).once.with(fixture.provider.get_create_alias_command,ops_hash).and_return(Puppet::Provider::Brocade_responses::RESPONSE_INVALID_NAME)
-	  expect {fixture.provider.create}.to raise_error(Puppet::Error)
+ 	 #When-Then
+      @fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_create_alias_command,NOOPS_HASH).and_return(Puppet::Provider::Brocade_responses::RESPONSE_INVALID_NAME)
+	  expect {@fixture.provider.create}.to raise_error(Puppet::Error)
 	end
 	
 	it "should raise error if response contains 'name too long' while creating brocade alias" do
-	 #Given
-      fixture = Brocade_alias_fixture.new
-	  ops_hash = {:noop => false}
-	  dummy_transport=double('transport')
-	  fixture.provider.transport = dummy_transport 
-	  
-	  #When-Then
-      dummy_transport.should_receive(:command).once.with(fixture.provider.get_create_alias_command,ops_hash).and_return(Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG)
-	  expect {fixture.provider.create}.to raise_error(Puppet::Error)	
+	 #When-Then
+      @fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_create_alias_command,NOOPS_HASH).and_return(Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG)
+	  expect {@fixture.provider.create}.to raise_error(Puppet::Error)	
 	end
 	
 	it "should warn if brocade alias name already exist" do
-	  fixture = Brocade_alias_fixture.new
-	  ops_hash = {:noop => false}
-	  dummy_transport=double('transport')
-	  fixture.provider.transport = dummy_transport 
+	 #Then
+      @fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_create_alias_command,NOOPS_HASH).and_return(Puppet::Provider::Brocade_responses::RESPONSE_DUPLICATE_NAME)
+      Puppet.should_receive(:info).once.with(Puppet::Provider::Brocade_messages::ALIAS_ALREADY_EXIST_INFO%[@fixture.get_alias_name])
 	  
-      #When-Then
-      dummy_transport.should_receive(:command).once.with(fixture.provider.get_create_alias_command,ops_hash).and_return(Puppet::Provider::Brocade_responses::RESPONSE_DUPLICATE_NAME)
-      Puppet.stub(:info)
-	  Puppet.should_receive(:info).once.with(Puppet::Provider::Brocade_messages::ALIAS_ALREADY_EXIST_INFO%[fixture.get_alias_name])
-	  
-	  #When
-	  fixture.provider.create
-	  
+	 #When
+	  @fixture.provider.create
 	  end
-	it "should create the brocade alias" 
+	  
+	it "should create the brocade alias" do
+     #Then
+      @fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_create_alias_command,NOOPS_HASH).and_return ("")
+	  @fixture.provider.should_receive(:cfg_save).once
+	  
+	 #When
+	  @fixture.provider.create	  
+	end
 	
   end
   
@@ -83,29 +76,6 @@ describe Puppet::Type.type(:brocade_alias).provider(:brocade_alias) do
 	it "should return false when the brocade alias does not exist" 	
   end
   
-  class Brocade_alias_fixture
-
-  attr_accessor :brocade_alias, :provider
-
-  def initialize
-    @brocade_alias = get_brocade_alias
-	@provider = brocade_alias.provider	
-  end
-  
-  private 
-  def  get_brocade_alias
-    Puppet::Type.type(:brocade_alias).new(
-    :alias_name => 'DemoAlias',
-    :ensure => 'present',
-    :member => '0f:0a:0a:0a:0a:0a:0a:0a'
-	)
-  end
-  
-  public
-  def get_alias_name
-    brocade_alias[:alias_name]
-  end
-  end
   
 end
 
