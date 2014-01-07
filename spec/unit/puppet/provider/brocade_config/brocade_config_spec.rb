b2399c6af5c0bc8af656c7d5ca2ea8f0ff0727d3
anuj mittal <anuj_mittal@dell.com>
2014-01-07 08:57:32 +0000

    Updated test cases.

--- spec/unit/puppet/provider/brocade_config/brocade_config_spec.rb
+++ spec/unit/puppet/provider/brocade_config/brocade_config_spec.rb
@@ -10,7 +10,17 @@ require 'puppet/provider/brocade_responses'
 require 'puppet/provider/brocade_messages'
 require 'fixtures/unit/puppet/provider/brocade_config/brocade_config_fixture'
 
+NOOP_HASH = { :noop => false}
+PROMPT_HASH = {:prompt => Puppet::Provider::Brocade_messages::CONFIG_ENABLE_PROMPT}
+
 describe Puppet::Type.type(:brocade_config).provider(:brocade_config) do
+
+  before(:each) do
+    @fixture = Brocade_config_fixture.new
+	  dummy_transport=double('transport')
+    @fixture.provider.transport = dummy_transport
+    @fixture.provider.stub(:cfg_save)
+  end
 
   context "when brocade_config provider is created " do
     it "should have create method defined for brocade_config" do
@@ -24,88 +34,67 @@ describe Puppet::Type.type(:brocade_config).provider(:brocade_config) do
 
     it "should have exists? method defined for brocade_config" do
       described_class.instance_method(:exists?).should_not == nil
-
+    end
+	
+	it "should have parent 'Puppet::Provider::Brocade_fos'" do
+      described_class.new.should be_kind_of(Puppet::Provider::Brocade_fos)
     end
   end
 
   context "when brocade config is created and/or its state is changed" do
   it "should warn if brocade config name already exist and process the brocade config state" do
-    #Given
-	fixture = Brocade_config_fixture.new
-	mock_transport=double('transport')
-    mock_transport.stub(:command).and_return ""    
-	fixture.provider.transport = mock_transport 
-	
 	#Then
-	resp = Puppet::Provider::Brocade_messages::CONFIG_ALREADY_EXIST%[fixture.get_config_name]	
-    Puppet.stub(:info)
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_show_command, NOOP_HASH).and_return ("")
+	resp = Puppet::Provider::Brocade_messages::CONFIG_ALREADY_EXIST%[@fixture.get_config_name]	
 	Puppet.should_receive(:info).once.with(resp)
-	fixture.provider.stub(:process_config_state)
+	@fixture.provider.should_receive(:process_config_state).once.and_return ("")
 	
 	#When
-    fixture.provider.create
+    @fixture.provider.create
   end
   
-  it "should create brocade config if brocade config name is not present and process the brocade config state" do
-        #Given
-  	fixture = Brocade_config_fixture.new
-	mock_transport=double('transport')
-        mock_transport.stub(:command) do |arg1, arg2|
-	  if arg1.include? "cfgshow"
-	    Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST
-	  elsif arg1.include? "cfgcreate"
-	    ""
-	  end
-	end	
-	
-	fixture.provider.transport = mock_transport	
-	fixture.provider.stub(:process_config_state)
-	
+  it "should create brocade config if brocade config name is not present and process the brocade config state" do	
 	#Then
-	fixture.provider.stub(:cfg_save)
-	fixture.provider.should_receive(:cfg_save).once
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_show_command, NOOP_HASH).and_return (Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_create_command, NOOP_HASH).and_return("")
+	@fixture.provider.should_receive(:cfg_save).once
+	@fixture.provider.should_receive(:process_config_state).once.and_return ("")
 	
 	#When
-	fixture.provider.create
+	@fixture.provider.create
   end
 	
-  it "should raise error if response contains 'invalid' while creating brocade config" do
-    #Given
-   	fixture = Brocade_config_fixture.new
-	mock_transport=double('transport')
-    mock_transport.stub(:command) do |arg1, arg2|
-	  if arg1.include? "cfgshow"
-	    Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST
-	  elsif arg1.include? "cfgcreate"
-	    Puppet::Provider::Brocade_responses::RESPONSE_INVALID
-	  end
-	end	
-	fixture.provider.transport = mock_transport 	
-	fixture.provider.stub(:process_config_state)
-	
+  it "should raise error if response contains 'invalid' while creating brocade config" do	
 	#When - Then
-    expect {fixture.provider.create}.to raise_error(Puppet::Error)
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_show_command, NOOP_HASH).and_return (Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_create_command, NOOP_HASH).and_return(Puppet::Provider::Brocade_responses::RESPONSE_INVALID)
+	
+    expect {@fixture.provider.create}.to raise_error(Puppet::Error)
   end
 	
    it "should raise error if response contains 'name too long' while creating brocade config" do
-    #Given
-   	fixture = Brocade_config_fixture.new
-	mock_transport=double('transport')
-    mock_transport.stub(:command) do |arg1, arg2|
-	  if arg1.include? "cfgshow"
-	    Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST
-	  elsif arg1.include? "cfgcreate"
-	    Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG
-	  end
-	end	
-	fixture.provider.transport = mock_transport 	
-	fixture.provider.stub(:process_config_state)
-	
 	#When - Then
-    expect {fixture.provider.create}.to raise_error(Puppet::Error)
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_show_command, NOOP_HASH).and_return (Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_create_command, NOOP_HASH).and_return(Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG)
+
+    expect {@fixture.provider.create}.to raise_error(Puppet::Error)
   end
    
-   it "should enable the brocade config state when brocade config state value is enabled in resource" 
+  it "should enable the brocade config state when brocade config state value is enabled in resource" do
+	#Given
+	Puppet.stub(:info)
+	@fixture.set_configstate_enable
+	
+	#Then
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_show_command, NOOP_HASH).and_return ("")
+	@fixture.provider.should_receive(:process_config_state).once.and_call_original
+	@fixture.provider.should_receive(:config_enable).once.and_call_original
+	@fixture.provider.transport.should_receive(:command).once.with(@fixture.provider.get_brocade_config_enable_command, PROMPT_HASH).and_return ("")
+	@fixture.provider.transport.should_receive(:command).once.with("yes", NOOP_HASH).and_return ("")
+	
+	#When
+	@fixture.provider.create
+  end
 	
    it "should raise error if response contains 'not found' while enabling the brocade config state"
    
