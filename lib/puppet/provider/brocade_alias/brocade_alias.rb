329b3e7bcd2c7a5dadc8ac46e3838a327404ace9
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2014-01-08 09:57:59 +0000

    externalizing the brocade commands

--- lib/puppet/provider/brocade_alias/brocade_alias.rb
+++ lib/puppet/provider/brocade_alias/brocade_alias.rb
@@ -1,27 +1,16 @@
 require 'puppet/provider/brocade_fos'
 require 'puppet/provider/brocade_responses'
 require 'puppet/provider/brocade_messages'
+require 'puppet/provider/brocade_commands'
 
 Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Provider::Brocade_fos) do
   @doc = "Manage brocade alias creation and deletion."
 
  mk_resource_methods
-
- def get_create_alias_command
-  return "alicreate #{@resource[:alias_name]}, \"#{@resource[:member]}\""
- end
- 
- def get_delete_alias_command
-   return "alidelete  #{@resource[:alias_name]}"
- end
- 
- def get_alias_show_command
-   return "alishow #{@resource[:alias_name]}"
- end
  
  def create
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_CREATE_DEBUG%[@resource[:alias_name],@resource[:member]])
-    response = @transport.command(get_create_alias_command, :noop => false)
+    response = @transport.command((Puppet::Provider::Brocade_commands::ALIAS_CREATE_COMMAND%[@resource[:alias_name],@resource[:member]]), :noop => false)
     if((response.include? Puppet::Provider::Brocade_responses::RESPONSE_NAME_TOO_LONG )|| (response.include? Puppet::Provider::Brocade_responses::RESPONSE_INVALID_NAME))
 	  raise Puppet::Error, Puppet::Provider::Brocade_messages::ALIAS_CREATE_ERROR%[@resource[:alias_name],response]
 	elsif(response.include? Puppet::Provider::Brocade_responses::RESPONSE_DUPLICATE_NAME)
@@ -33,7 +22,7 @@ Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Pro
 
   def destroy
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_DESTROY_DEBUG%[@resource[:alias_name]])
-    response = @transport.command(get_delete_alias_command, :noop => false)
+    response = @transport.command(Puppet::Provider::Brocade_commands::ALIAS_DELETE_COMMAND%[@resource[:alias_name]], :noop => false)
     if (response.include? Puppet::Provider::Brocade_responses::RESPONSE_NOT_FOUND)
 	 Puppet.info(Puppet::Provider::Brocade_messages::ALIAS_DOES_NOT_EXIST_INFO%[@resource[:alias_name]])
     else
@@ -44,7 +33,7 @@ Puppet::Type.type(:brocade_alias).provide(:brocade_alias, :parent => Puppet::Pro
   def exists?
     Puppet.debug(Puppet::Provider::Brocade_messages::ALIAS_EXIST_DEBUG%[@resource[:alias_name]])
     self.device_transport
-    response = @transport.command(get_alias_show_command, :noop => false)
+    response = @transport.command(Puppet::Provider::Brocade_commands::ALIAS_SHOW_COMMAND%[@resource[:alias_name]], :noop => false)
     if !(response.include? Puppet::Provider::Brocade_responses::RESPONSE_DOES_NOT_EXIST)
       true
     else
---
+++ lib/puppet/provider/brocade_commands.rb
@@ -0,0 +1,5 @@
+module Puppet::Provider::Brocade_commands
+  ALIAS_CREATE_COMMAND = "alicreate %s, %s"
+  ALIAS_DELETE_COMMAND = "alidelete %s"
+  ALIAS_SHOW_COMMAND = "alishow %s"
+end
