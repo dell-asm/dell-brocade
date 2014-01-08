c3c3c09c5eabc11fdcfb5315692ac76fbff94469
sanjeev rohila <sanjeev_rohila@dell.com>
2014-01-08 10:05:29 +0000

    updating spec_helper for integrations test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,106 @@
-require 'rspec-puppet'
-require 'spec_helper'
-require 'yaml'
-require 'puppet/provider/brocade_fos'
-require 'puppet/util/network_device/brocade_fos/device'
-require 'puppet/util/network_device/base_fos'
-require 'rspec/mocks'
-require 'puppet/provider/brocade_fos'
-require 'puppet/provider/brocade_responses'
-require 'puppet/provider/brocade_messages'
+# NOTE: a lot of the stuff in this file is duplicated in the "puppet_spec_helper" in the project
+#  puppetlabs_spec_helper.  We should probably eat our own dog food and get rid of most of this from here,
+#  and have the puppet core itself use puppetlabs_spec_helper
 
-module_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..','lib'))
+dir = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift File.join(dir, 'spec_lib')
 
-RSpec.configure do |c|
-  c.module_path = module_path
-  c.manifest_dir = File.join(module_path, 'manifests')
+# Don't want puppet getting the command line arguments for rake or autotest
+ARGV.clear
+
+begin
+  require 'rubygems'
+rescue LoadError
+end
+
+require 'puppet'
+gem 'rspec', '>=2.0.0'
+require 'rspec/expectations'
+
+# So everyone else doesn't have to include this base constant.
+module PuppetSpec
+  FIXTURE_DIR = File.join(dir = File.expand_path(File.dirname(__FILE__)), "fixtures") unless defined?(FIXTURE_DIR)
+end
+
+require 'pathname'
+require 'tmpdir'
+require 'fileutils'
+
+require 'puppet_spec/verbose'
+require 'puppet_spec/files'
+require 'puppet_spec/settings'
+require 'puppet_spec/fixtures'
+require 'puppet_spec/deviceconf'
+require 'puppet_spec/matchers'
+require 'puppet_spec/database'
+require 'monkey_patches/alias_should_to_must'
+require 'puppet/test/test_helper'
+
+RSpec.configure do |config|
+  include PuppetSpec::Fixtures
+  include PuppetSpec::Deviceconf
+
+  config.filter_run_excluding :broken => true
+
+  config.mock_with :mocha
+
+  tmpdir = Dir.mktmpdir("rspecrun")
+  oldtmpdir = Dir.tmpdir()
+  ENV['TMPDIR'] = tmpdir
 
   if Puppet::Util::Platform.windows?
-    c.output_stream = $stdout
-    c.error_stream = $stderr
+    config.output_stream = $stdout
+    config.error_stream = $stderr
 
-    c.formatters.each do |f|
+    config.formatters.each do |f|
       if not f.instance_variable_get(:@output).kind_of?(::File)
         f.instance_variable_set(:@output, $stdout)
       end
     end
+  end
+
+  Puppet::Test::TestHelper.initialize
+
+  config.before :all do
+    Puppet::Test::TestHelper.before_all_tests()
+  end
+
+  config.after :all do
+    Puppet::Test::TestHelper.after_all_tests()
+  end
+
+  config.before :each do
+    GC.disable
+    Signal.stubs(:trap)
+
+    @logs = []
+    Puppet::Util::Log.newdestination(Puppet::Test::LogCollector.new(@logs))
+
+    @log_level = Puppet::Util::Log.level
+
+    Puppet::Test::TestHelper.before_each_test()
+
+  end
+
+  config.after :each do
+    Puppet::Test::TestHelper.after_each_test()
+
+    PuppetSpec::Files.cleanup
+
+    @logs.clear
+    Puppet::Util::Log.close_all
+    Puppet::Util::Log.level = @log_level
+
+    GC.enable
+  end
+
+  config.after :suite do
+    if ENV['LOG_SPEC_ORDER']
+      File.open("./spec_order.txt", "w") do |logfile|
+        config.instance_variable_get(:@files_to_run).each { |f| logfile.puts f }
+      end
+    end
+    ENV['TMPDIR'] = oldtmpdir
+    FileUtils.rm_rf(tmpdir) if File.exists?(tmpdir) && tmpdir.to_s.start_with?(oldtmpdir)
   end
 end
