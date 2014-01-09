4d935ffc70abd637fe34d88af5dbc0e4516d0b94
sanjeev rohila <sanjeev_rohila@dell.com>
2014-01-09 11:45:04 +0000

    modified test for zone create and delete integration test.

--- spec/integration/puppet/provider/brocade_zone/brocade_zone_spec.rb
+++ spec/integration/puppet/provider/brocade_zone/brocade_zone_spec.rb
@@ -2,17 +2,12 @@
 
 require 'spec_helper'
 require 'yaml'
-require 'rspec/expectations'
-require 'puppet/provider/brocade_fos'
 require 'puppet/util/network_device/brocade_fos/device'
-require 'puppet/util/network_device'
-require 'rspec/mocks'
-require 'puppet/provider/brocade_responses'
-require 'puppet/provider/brocade_messages'
+require 'puppet/provider/brocade_fos'
 require 'puppet/util/network_device/transport_fos/ssh'
 
 
-describe Puppet::Type.type(:brocade_zone).provider(:brocade_zone) do
+describe "Integration test for brocade zone create and destroy" do
 
   device_conf =  YAML.load_file(my_deviceurl('brocade','device_conf.yml'))
 
@@ -35,52 +30,43 @@ describe Puppet::Type.type(:brocade_zone).provider(:brocade_zone) do
     )
   end
   
-  def get_zoneshow
-    command = "zoneshow #{create_zone[:zonename]}"
-  end   
+  def get_zoneshow_cmd(zonename)
+    command = "zoneshow #{zonename}"    
+  end
 
-  context "Create and Destroy Zone" do
+  context "should Create and Destroy Zone" do
     it "should create a brocade zone" do
-      type = destroy_zone
-      type_provider = type.provider
-      type_provider.device_transport.connect
+      destroy_zone.provider.device_transport.connect
       ##Initially check the presense of the zone,if exist destroy the existing zone
       ##and continue the create zone feature of the brocade_zone provider.
-      zone_show_res = type_provider.device_transport.command(get_zoneshow,:noop=>false)
-      zone_absent = presense?(zone_show_res,"does not exist")
-      if zone_absent == false
+      zone_show_res = destroy_zone.provider.device_transport.command(get_zoneshow_cmd(create_zone[:zonename]),:noop=>false)
+      if presense?(zone_show_res,"does not exist") == false
         ##if zone already exist, will delete the zone and continue with the test.
-        zone_del_res = type_provider.device_transport.command("zonedelete #{create_zone[:zonename]}",:noop=>false)
-        cfg_save_res =  type_provider.device_transport.command("cfgsave", :prompt => /Do/)
+        zone_del_res = destroy_zone.provider.device_transport.command("zonedelete #{create_zone[:zonename]}",:noop=>false)
+        cfg_save_res = destroy_zone.provider.device_transport.command("cfgsave", :prompt => /Do/)
         if cfg_save_res.match(/fail|err|not found|not an alias/)
           puts "Unable to save the Config because of the following issue: #{cfg_save_res}"
         else
           puts "Successfully saved the Config"
         end
       end 
-      type_provider.device_transport.close
-      type = create_zone
-      type_provider = type.provider
-      type_provider.device_transport.connect
-      type_provider.create
-      create_response = type_provider.device_transport.command(get_zoneshow,:noop=>false)
-      type_provider.device_transport.close
+      destroy_zone.provider.device_transport.close
+      create_zone.provider.device_transport.connect
+      create_zone.provider.create
+      create_response = create_zone.provider.device_transport.command(get_zoneshow_cmd(create_zone[:zonename]),:noop=>false)
+      create_zone.provider.device_transport.close
       create_response.should_not include("does not exist")
     end
     
     it "should destroy a brocade zone" do
-      type = create_zone
-      type_provider = type.provider
-      type_provider.device_transport.connect
-      type.provider.create
+      create_zone.provider.device_transport.connect
+      create_zone.provider.create
 
-      type_provider.device_transport.close
+      create_zone.provider.device_transport.close
 
-      type = destroy_zone
-      type_provider = type.provider
-      type_provider.device_transport.connect
-      type.provider.destroy
-      destroy_response = type_provider.device_transport.command(get_zoneshow,:noop=>false)
+      destroy_zone.provider.device_transport.connect
+      destroy_zone.provider.destroy
+      destroy_response = destroy_zone.provider.device_transport.command(get_zoneshow_cmd(destroy_zone[:zonename]),:noop=>false)
       destroy_response.should include("does not exist")
     end
 
