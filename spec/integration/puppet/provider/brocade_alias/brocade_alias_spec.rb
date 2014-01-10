1a0f42bc60f3613c3a8aa9f9e1e0a3e287b7a373
sangeeta sharma <sangeeta_sharma@dell.com>
2014-01-10 10:48:37 +0000

    Integration test for brocade alias

--- spec/integration/puppet/provider/brocade_alias/brocade_alias_spec.rb
+++ spec/integration/puppet/provider/brocade_alias/brocade_alias_spec.rb
@@ -6,13 +6,12 @@ require 'puppet/util/network_device/brocade_fos/device'
 require 'puppet/provider/brocade_fos'
 require 'puppet/util/network_device/transport_fos/ssh'
 
-describe Puppet::Type.type(:brocade_alias).provider(:brocade_alias), '(integration)' do
+describe "Integration test for brocade alias" do
 
   device_conf =  YAML.load_file(my_deviceurl('brocade','device_conf.yml'))
 
   before :each do
     Facter.stubs(:value).with(:url).returns(device_conf['url'])
-    described_class.stubs(:suitable?).returns true
   end
 
   let :create_alias do
@@ -30,16 +29,24 @@ describe Puppet::Type.type(:brocade_alias).provider(:brocade_alias), '(integrati
   end
 
   context "when create and delete alias without any error" do
-    it "should be create a brocade alias" do
+    it "should create a brocade alias" do
       create_alias.provider.device_transport.connect
+      response = create_alias.provider.device_transport.command(get_alias_show_cmd(create_alias[:alias_name]),:noop=>false)
+      if !response.include?("does not exist")
+        destroy_alias.provider.device_transport.connect
+        destroy_alias.provider.destroy
+        destroy_alias.provider.device_transport.close
+      end
       create_alias.provider.create
       response = create_alias.provider.device_transport.command(get_alias_show_cmd(create_alias[:alias_name]),:noop=>false)
+      create_alias.provider.device_transport.close
       response.should_not include("does not exist")
     end
     it "should delete the brocade alias" do
       destroy_alias.provider.device_transport.connect
       destroy_alias.provider.destroy
       response = destroy_alias.provider.device_transport.command(get_alias_show_cmd(create_alias[:alias_name]),:noop=>false)
+      destroy_alias.provider.device_transport.close
       response.should include("does not exist")
     end
   end
