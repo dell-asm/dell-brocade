715f89b9a3874b04951ad3176647f9e2b15a371b
sanjeev rohila <sanjeev_rohila@dell.com>
2014-01-10 12:34:34 +0000

    brocade_zone_spec.rb is modified as per review comments.

--- spec/integration/puppet/provider/brocade_zone/brocade_zone_spec.rb
+++ spec/integration/puppet/provider/brocade_zone/brocade_zone_spec.rb
@@ -32,35 +32,30 @@ describe "Integration test for brocade zone create and destroy" do
   def get_zoneshow_cmd(zonename)
     command = "zoneshow #{zonename}"
   end
+  
+  
 
   context "should Create and Destroy Zone" do
     it "should create a brocade zone" do
       destroy_zone.provider.device_transport.connect
-      ##Initially check the presense of the zone,if exist destroy the existing zone
-      ##and continue the create zone feature of the brocade_zone provider.
-      zone_show_res = destroy_zone.provider.device_transport.command(get_zoneshow_cmd(create_zone[:zonename]),:noop=>false)
-      if presense?(zone_show_res,"does not exist") == false
-        ##if zone already exist, will delete the zone and continue with the test.
-        zone_del_res = destroy_zone.provider.device_transport.command("zonedelete #{create_zone[:zonename]}",:noop=>false)
-        cfg_save_res = destroy_zone.provider.device_transport.command("cfgsave", :prompt => /Do/)
-        if cfg_save_res.match(/fail|err|not found|not an alias/)
-          puts "Unable to save the Config because of the following issue: #{cfg_save_res}"
-        else
-          puts "Successfully saved the Config"
-        end
-      end
+      destroy_zone.provider.destroy
       destroy_zone.provider.device_transport.close
+      
       create_zone.provider.device_transport.connect
       create_zone.provider.create
       create_response = create_zone.provider.device_transport.command(get_zoneshow_cmd(create_zone[:zonename]),:noop=>false)
-      create_response.should_not include("does not exist")
       create_zone.provider.device_transport.close
+      
+      destroy_zone.provider.device_transport.connect
+      destroy_zone.provider.destroy
+      destroy_zone.provider.device_transport.close
+      
+      create_response.should_not include("does not exist")
     end
 
     it "should be able to destroy a brocade zone" do
       create_zone.provider.device_transport.connect
       create_zone.provider.create
-
       create_zone.provider.device_transport.close
 
       destroy_zone.provider.device_transport.connect
