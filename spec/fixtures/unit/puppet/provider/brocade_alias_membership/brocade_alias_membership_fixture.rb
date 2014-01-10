c2d4e37885595da44186db045bf15a549eb03e7f
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2014-01-10 09:59:51 +0000

    Ficture changes after munge functionality addition in type

--- spec/fixtures/unit/puppet/provider/brocade_alias_membership/brocade_alias_membership_fixture.rb
+++ spec/fixtures/unit/puppet/provider/brocade_alias_membership/brocade_alias_membership_fixture.rb
@@ -10,30 +10,27 @@ class Brocade_alias_membership_fixture
 
   def  get_brocade_alias_membership
     Puppet::Type.type(:brocade_alias_membership).new(
-    :alias_name => 'DemoAlias',
-    :ensure => 'present',
-    :member => '0f:0f:0f:0f:0f:0f:0f:0f ',
+    :name => 'DemoAlias:0f:0f:0f:0f:0f:0f:0f:0f ',
+    :ensure => 'present'
     )
   end
 
-end
-
-class Brocade_alias_membership_fixture_with_absent
-
-  attr_accessor :brocade_alias_membership, :provider
-  def initialize
-    @brocade_alias_membership = get_brocade_alias_membership
-    @provider = brocade_alias_membership.provider
+public
+  def  get_alias_name
+    @brocade_alias_membership[:alias_name]
+  end
+  
+  def  get_member_name
+    @brocade_alias_membership[:member]
   end
 
-  private
 
-  def  get_brocade_alias_membership
-    Puppet::Type.type(:brocade_alias_membership).new(
-    :alias_name => 'DemoAlias',
-    :ensure => 'absent',
-    :member => '0f:0f:0f:0f:0f:0f:0f:0f ',
-    )
+  def  get_ensure_value
+    @brocade_alias_membership[:ensure]
+  end
+  
+  def  set_ensure_value_absent
+    @brocade_alias_membership[:ensure] = 'absent'
   end
 
 end
