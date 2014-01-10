bf229402983df27cbc262cdf8f6b45001900cfe6
sangeeta sharma <sangeeta_sharma@dell.com>
2014-01-10 13:07:58 +0000

    Integration test for brocade alias membership

--- spec/integration/puppet/provider/brocade_alias_membership/brocade_alias_membership_spec.rb
+++ spec/integration/puppet/provider/brocade_alias_membership/brocade_alias_membership_spec.rb
@@ -10,10 +10,6 @@ describe "Integration test for brocade alias membership" do
 
   device_conf =  YAML.load_file(my_deviceurl('brocade','device_conf.yml'))
 
-  before :each do
-    Facter.stubs(:value).with(:url).returns(device_conf['url'])
-  end
-
   let :create_alias do
     Puppet::Type.type(:brocade_alias).new(
     :alias_name  => 'testalias2',
@@ -29,7 +25,7 @@ describe "Integration test for brocade alias membership" do
     )
   end
   let :remove_members do
-    Puppet::Type.type(:brocade_alias).new(
+    Puppet::Type.type(:brocade_alias_membership).new(
     :alias_name  => 'testalias2',
     :ensure   => 'absent',
     :member   => '1f:2a:0a:0a:0a:0a:0a:0b',
@@ -41,32 +37,38 @@ describe "Integration test for brocade alias membership" do
     :ensure   => 'absent',
     )
   end
+  #Before every test alias will be created
+  before :each do
+    Facter.stubs(:value).with(:url).returns(device_conf['url'])
+    create_alias.provider.device_transport.connect
+    create_alias.provider.create
+    create_alias.provider.device_transport.close    
+  end
+  
+  #After every test alias will be destroyed
+  after :each do
+    destroy_alias.provider.device_transport.connect
+    destroy_alias.provider.destroy
+    destroy_alias.provider.device_transport.close
+  end
 
   context "when add and remove alias members without any error" do
-    it "should create a brocade alias" do
-      create_alias.provider.device_transport.connect
-      create_alias.provider.create
-      response = create_alias.provider.device_transport.command(get_alias_show_cmd(create_alias[:alias_name]),:noop=>false)
-      response.should_not include("does not exist")
-    end
+        
     it "should add a member brocade alias" do
       add_members.provider.device_transport.connect
       add_members.provider.create
       response = add_members.provider.device_transport.command(get_alias_show_cmd(add_members[:alias_name]),:noop=>false)
+      add_members.provider.device_transport.close
       response.should_not include("does not exist")
     end
+    
     it "should delete member from brocade alias" do
       remove_members.provider.device_transport.connect
       remove_members.provider.destroy
       response = remove_members.provider.device_transport.command(get_alias_show_cmd(remove_members[:alias_name]),:noop=>false)
-    # response.should include("does not exist")
-    end
-    it "should delete the brocade alias" do
-      destroy_alias.provider.device_transport.connect
-      destroy_alias.provider.destroy
-      response = destroy_alias.provider.device_transport.command(get_alias_show_cmd(destroy_alias[:alias_name]),:noop=>false)
-      response.should include("does not exist")
-    end
+      remove_members.provider.device_transport.close      
+      response.should_not include(remove_members[:member])
+    end   
   end
 
   def get_alias_show_cmd(aliasname)
