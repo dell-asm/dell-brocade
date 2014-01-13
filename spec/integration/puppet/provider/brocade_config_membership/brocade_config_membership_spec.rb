bd4e82ea4692333dfb2058ef69b0efa3bc477303
sanjeev rohila <sanjeev_rohila@dell.com>
2014-01-13 18:05:27 +0000

    Re-factored the code for brocade_config_membership and also modified the code for latest requirements as per the modified providers.

--- spec/integration/puppet/provider/brocade_config_membership/brocade_config_membership_spec.rb
+++ spec/integration/puppet/provider/brocade_config_membership/brocade_config_membership_spec.rb
@@ -1,10 +1,8 @@
 #! /usr/bin/env ruby
-
 require 'spec_helper'
 require 'yaml'
-require 'puppet/util/network_device/brocade_fos/device'
-require 'puppet/provider/brocade_fos'
-require 'puppet/util/network_device/transport_fos/ssh'
+require 'spec_lib/puppet_spec/deviceconf'
+include PuppetSpec::Deviceconf
 
 describe "Integration Testing for Brocade config" do
 
@@ -19,16 +17,14 @@ describe "Integration Testing for Brocade config" do
 
   let :add_zone_config do
     Puppet::Type.type(:brocade_config_membership).new(
-    :configname  => 'DemoConfig',
-    :member_zone   => 'DemoNewZone',
+    :name  => 'DemoConfig:DemoNewZone',
     :ensure => 'present'
     )
   end
 
   let :remove_zone_config do
     Puppet::Type.type(:brocade_config_membership).new(
-    :configname  => 'DemoConfig',
-    :member_zone   => 'DemoNewZone',
+    :name  => 'DemoConfig:DemoNewZone',
     :ensure => 'absent'
     )
   end
@@ -40,7 +36,7 @@ describe "Integration Testing for Brocade config" do
 
 #Before every test config will be created
   before :each do
-    Facter.stubs(:value).with(:url).returns(device_conf['url'])
+    Facter.stub(:value).with(:url).and_return(device_conf['url'])
     create_config.provider.device_transport.connect
     create_config.provider.create
     create_config.provider.device_transport.close
@@ -58,20 +54,20 @@ describe "Integration Testing for Brocade config" do
       add_zone_config.provider.device_transport.connect
       add_zone_config.provider.create
       response = add_zone_config.provider.device_transport.command(get_brocade_config_show_command(add_zone_config[:configname]),:noop=>false)
-      response.should include(add_zone_config[:member_zone])
+      zone_name = get_zone_name(add_zone_config[:name])
       add_zone_config.provider.device_transport.close
-
+      puts "Create - Checking presense of zone_name #{zone_name} in CFG -#{response} "
+      presense?(response,zone_name).should == true
     end
 
     it "should be able to remove the zone from config" do
-      add_zone_config.provider.device_transport.connect
-      add_zone_config.provider.create
-      add_zone_config.provider.device_transport.close
       remove_zone_config.provider.device_transport.connect
       remove_zone_config.provider.destroy
       response = remove_zone_config.provider.device_transport.command(get_brocade_config_show_command(remove_zone_config[:configname]),:noop=>false)
-      response.should_not include(remove_zone_config[:member_zone])
+      zone_name = get_zone_name(remove_zone_config[:name])
       remove_zone_config.provider.device_transport.close
+      puts "Destroy - Checking presense of zone_name #{zone_name} in CFG -#{response} "
+      presense?(response,zone_name).should_not == true
     end
   end
 
@@ -79,8 +75,19 @@ describe "Integration Testing for Brocade config" do
     command = "cfgshow #{configname}"
   end
 
-  def get_brocade_zone_show_command
-    command = "cfgactvshow"
+  def presense?(response_string,key_to_check)
+    retval = false
+    if response_string.include?("#{key_to_check}")
+    retval = true
+    else
+    retval = false
+    end
+    return retval
+  end
+
+  def get_zone_name(inputString)
+    member = inputString.split(':')
+    return member[1]
   end
 
 end
