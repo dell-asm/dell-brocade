01708d8bc2e2d71096dc4e935b4559fff1151c02
sanjeev rohila <sanjeev_rohila@dell.com>
2014-01-13 11:48:18 +0000

    Changed to entertain the unit and integrations tests both.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,13 @@ require 'puppet/provider/brocade_fos'
 require 'puppet/util/network_device/brocade_fos/device'
 require 'puppet/util/network_device/base_fos'
 require 'rspec/mocks'
-require 'puppet/provider/brocade_fos'
 require 'puppet/provider/brocade_responses'
 require 'puppet/provider/brocade_messages'
 require 'puppet/provider/brocade_commands'
+require 'spec_lib/puppet_spec/deviceconf'
+require 'rspec/expectations'
+
+
 
 module_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..','lib'))
@@ -16,7 +19,6 @@ $LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), '..','lib'
 RSpec.configure do |c|
   c.module_path = module_path
   c.manifest_dir = File.join(module_path, 'manifests')
-
   if Puppet::Util::Platform.windows?
     c.output_stream = $stdout
     c.error_stream = $stderr
