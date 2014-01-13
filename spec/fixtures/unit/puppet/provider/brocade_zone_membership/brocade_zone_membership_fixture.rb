4bb8815c87d34288b8c04d8de6b69c5ebad3938c
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2014-01-13 04:54:09 +0000

    Changes done according to new param added in  type defination

--- spec/fixtures/unit/puppet/provider/brocade_zone_membership/brocade_zone_membership_fixture.rb
+++ spec/fixtures/unit/puppet/provider/brocade_zone_membership/brocade_zone_membership_fixture.rb
@@ -10,30 +10,26 @@ class Brocade_zone_membership_fixture
 
   def  get_brocade_zone_membership
     Puppet::Type.type(:brocade_zone_membership).new(
-    :zonename => 'DemoZone',
-    :ensure => 'present',
-    :member => 'DemoMember',
+    :name => 'DemoZone:DemoMember',
+    :ensure => 'present'
     )
   end
-
-end
-
-class Brocade_zone_membership_fixture_with_absent
-
-  attr_accessor :brocade_zone_membership, :provider
-  def initialize
-    @brocade_zone_membership = get_brocade_zone_membership
-    @provider = brocade_zone_membership.provider
+  
+  public
+  def  get_zone_name
+    @brocade_zone_membership[:zonename]
+  end
+  
+  def  get_member_name
+    @brocade_zone_membership[:member]
   end
 
-  private
 
-  def  get_brocade_zone_membership
-    Puppet::Type.type(:brocade_zone_membership).new(
-    :zonename => 'DemoZone',
-    :ensure => 'absent',
-    :member => 'DemoMember',
-    )
+  def  get_ensure_value
+    @brocade_zone_membership[:ensure]
   end
-
+  
+  def  set_ensure_value_absent
+    @brocade_zone_membership[:ensure] = 'absent'
+  end
 end
