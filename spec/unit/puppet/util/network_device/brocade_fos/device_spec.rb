de2676d3654dd145b7c19997ea6e34799a014ad3
Sumanjeet Bhatti <Sumanjeet_Bhatti@dell.com>
2014-01-15 05:21:34 +0000

    Added some new unit test cases for device class

--- spec/unit/puppet/util/network_device/brocade_fos/device_spec.rb
+++ spec/unit/puppet/util/network_device/brocade_fos/device_spec.rb
@@ -7,38 +7,37 @@ describe "Brocade Device" do
     transport = double('transport')
     transport.stub(:is_a?).and_return 'true'
     transport.stub(:command).and_return ''
-
-    @fixture = Puppet::Util::NetworkDevice::Brocade_fos::Device.new(@device_url)
+    @fixture =Puppet::Util::NetworkDevice::Brocade_fos::Device.new(@device_url)
     @fixture.transport = transport
     @fixture.stub(:super).and_return("")
-  #   @fixture.stub(:initialize).and_return("")
+
   end
 
   context 'when creating the device' do
 
-    context "when brocade Zone Membership provider is created " do
+    context "when brocade device is created " do
 
       it "should have parent 'Puppet::Provider::Brocade_fos'" do
         @fixture.should be_kind_of(Puppet::Util::NetworkDevice::Base_fos)
       end
 
-      it "should have create method defined for brocade Zone Membership" do
+      it "should have connect transport method defined for brocade device" do
         @fixture.class.instance_method(:connect_transport).should_not == nil
       end
 
-      it "should have destroy method defined for brocade Zone Membership" do
+      it "should have login method defined for brocade device" do
         @fixture.class.instance_method(:login).should_not == nil
       end
 
-      it "should have exists? method defined for brocade_zone_membership" do
+      it "should have init method defined for brocade device" do
         @fixture.class.instance_method(:init).should_not == nil
       end
 
-      it "should have exists? method defined for brocade_zone_membership" do
+      it "should have init facts method defined for brocade device" do
         @fixture.class.instance_method(:init_facts).should_not == nil
       end
 
-      it "should have exists? method defined for brocade_zone_membership" do
+      it "should have facts method defined for brocade device" do
         @fixture.class.instance_method(:facts).should_not == nil
       end
 
@@ -57,5 +56,54 @@ describe "Brocade Device" do
       end
 
     end
+
+    context "should be able to connect and login the device" do
+
+      it " allows connection on the device and login " do
+        @fixture.transport.should_receive(:connect).once
+        @fixture.should_receive(:login).once
+        @fixture.connect_transport
+      end
+
+    end
+
+    context "should be able to validate and login" do
+
+      it " should prompt for the password, when username is given " do
+        @fixture.transport.should_receive(:handles_login?).once.and_return(false)
+        @fixture.transport.should_receive(:command).once.with(@fixture.url.user, {:prompt => /^Password:/, :noop => false})
+        @fixture.transport.should_receive(:command).once.with(@fixture.url.password, :noop => false)
+        @fixture.login
+      end
+
+      it "should give Password regex, when username is not given " do
+        @fixture.transport.should_receive(:handles_login?).once.and_return(false)
+        @fixture.url.stub(:user).and_return ''
+        @fixture.transport.should_not_receive(:command).with(@fixture.url.user, {:prompt => /^Password:/, :noop => false})
+        @fixture.transport.should_receive(:expect).with(/^Password:/).once
+        @fixture.transport.should_receive(:command).once.with(@fixture.url.password, :noop => false)
+
+        @fixture.login
+      end
+
+      it "should not perform any processing, when transport handles the login " do
+        @fixture.transport.should_receive(:handles_login?).once.and_return(true)
+        @fixture.transport.should_not_receive(:command).with(@fixture.url.user, {:prompt => /^Password:/, :noop => false})
+        @fixture.transport.should_not_receive(:command).with(@fixture.url.password, :noop => false)
+        @fixture.login
+      end
+
+    end
+
+    context "should initialize connection and facts retreive " do
+
+      it "should call connect transport method once" do
+        @fixture.should_receive(:connect_transport).once.ordered
+        @fixture.should_receive(:init_facts).once.ordered
+        @fixture.init
+      end
+
+    end
+
   end
 end
