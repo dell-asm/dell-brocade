e5152adfde2b11a99d08697a8a407f71f8a6eef7
Roland Pienaar <rip@devco.net>
2015-03-17 07:50:19 -0500

    ASM-3398 add credential_id to device config files
    Adds optional support for the new credential_id scheme

--- lib/puppet/util/network_device/base_fos.rb
+++ lib/puppet/util/network_device/base_fos.rb
@@ -11,55 +11,58 @@ class Puppet::Util::NetworkDevice::Base_fos
   attr_accessor :url, :transport, :crypt
   def initialize(url)
     @url = URI.parse(url)
-    query_str = @url.query
+    @query = {}
+    @query = CGI.parse(@url.query) if @url.query
+
     url_scheme = @url.scheme
-    @query = CGI.parse(query_str) if query_str
+
     require "puppet/util/network_device/transport_fos/#{url_scheme}"
 
     unless @transport
       @transport = Puppet::Util::NetworkDevice::Transport_fos.const_get(url_scheme.capitalize).new
       @transport.host = @url.host
-      @transport.port = get_transport_port()
-      update_transport_usr_password()
+      @transport.port = get_transport_port
+
+      update_transport_usr_password
     end
   end
 
   private
 
   def update_transport_usr_password
-
     if query_present_and_crypted?
       update_transport_for_crypted_url
     else
       update_transport_for_uncrypted_url
     end
-
+    
+    override_using_credential_id
   end
 
-  private
+  def override_using_credential_id
+    if id = @query.fetch('credential_id', []).first
+      require 'asm/cipher'
+      cred = ASM::Cipher.decrypt_credential(id)
+      @transport.user = cred.username
+      @transport.password = cred.password
+    end
+  end
 
   def update_transport_for_crypted_url
-
     self.crypt = true
     # FIXME: https://github.com/puppetlabs/puppet/blob/master/lib/puppet/application/device.rb#L181
     master = File.read(File.join('/etc/puppet', 'networkdevice-secret'))
     master = master.strip
     @transport.user = self.decrypt(master, [@url.user].pack('h*'))
     @transport.password = self.decrypt(master, [@url.password].pack('h*'))
-
   end
-
-  private
 
   def update_transport_for_uncrypted_url
     @transport.user = URI.decode(@url.user)
     @transport.password = URI.decode(asm_decrypt(@url.password))
   end
 
-  private
-
   def get_transport_port
-
     new_port = 22
     case @url.scheme
     when "ssh"
@@ -69,13 +72,9 @@ class Puppet::Util::NetworkDevice::Base_fos
     end
 
     @url.port || new_port
-
   end
 
-  private
-
   def query_present_and_crypted?
-
     if @query
       if @query['crypt'] == ['true']
       return true
@@ -83,8 +82,6 @@ class Puppet::Util::NetworkDevice::Base_fos
     end
     return false
   end
-
-  private
 
   def self.decrypt(master, str)
     cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
