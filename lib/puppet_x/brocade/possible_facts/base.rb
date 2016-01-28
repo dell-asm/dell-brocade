c884309644bc2dc9d42e573be913fbede76b22f9
Sushil_R <Sushil_R@Dell.com>
2016-01-28 09:39:49 +0530

    ASM-5692 Brocade Switch Discovery Failing on Build 5587
    - Pattern used for finding the zone information was not optimized
    - Skip zones where we done have any members in it

--- lib/puppet_x/brocade/possible_facts/base.rb
+++ lib/puppet_x/brocade/possible_facts/base.rb
@@ -228,8 +228,9 @@ module PuppetX::Brocade::PossibleFacts
           switch_name = base.facts['Switch Name'].value
           zones.split(',').each do |zone_val|
             zone_val.strip!
-            pattern = "zone:\s+#{zone_val}(.*?)(zone:|alias:|Effective configuration:)"
+            pattern = "zone:\\s+#{zone_val}(.*?)(zone:|alias:|Effective configuration:)"
             zone_detail = txt.scan(/#{pattern}/m)
+            next if zone_detail.nil? || zone_detail.empty?
             output = zone_detail[0].flatten.first.strip
             zone_members[zone_val] = []
             output.split("\n").each do |line|
